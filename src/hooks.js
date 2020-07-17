6853c148b7d98b29069c1ec7eaabb75f308cceb1
jaemin.han <jaemin.h15@gmail.com>
2020-07-16 22:52:04 -0400

    hook initial setup

---
+++ src/hooks.js
@@ -0,0 +1,15 @@
+import { useEffect, userRef, useState } from 'react';
+
+const userHover  = () => {
+  const ref = useRef();
+  const [ hovered, setHovered ] = useState(false); 
+  
+  useEffect(() => {
+    return () =>  {
+      
+    }
+  })
+  return [ref, hovered]
+};
+
+export default userHover;
