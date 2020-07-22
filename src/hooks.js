9f535a648456b2d5ea2f92b6785c67d6a70cbdac
jaemin.han <jaemin.h15@gmail.com>
2020-07-21 23:05:02 -0400

    update hooks files to use useEffect

--- src/hooks.js
+++ src/hooks.js
@@ -1,12 +1,24 @@
-import { useEffect, userRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const userHover  = () => {
   const ref = useRef();
   const [ hovered, setHovered ] = useState(false); 
   
+  // declare a function enter();
+  const enter = () => setHovered(true);
+  const leave = () => setHovered(false);
+
+  // passing in anonymous function as an argument
   useEffect(() => {
-    return () =>  {}
+    const refCopy = ref;
+    refCopy.current.addEventLister('mouseenter', enter);
+    refCopy.current.addEventLister('mouseleave', leave);
+    return () =>  {
+      refCopy.current.removeEventListener('mouseenter', enter);
+      refCopy.current.removeEventListener('mouseenter', leave);
+    }
   })
+
   return [ref, hovered]
 };
 
